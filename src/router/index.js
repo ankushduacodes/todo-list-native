76083ba88717bd95a4aab7ef8a65b356cdecb3aa
Ankush Dua <ankushduacodes@gmail.com>
2021-12-27 17:04:17 +0530

    added routes in accordance with the needs of the app

--- src/router/index.js
+++ src/router/index.js
@@ -8,13 +8,39 @@ const routes = [
   {
     path: '/myday',
     name: 'My day',
-    component: () => import('../views/MyDayPage.vue'),
+    component: () => import(/* webpackChunkName: "myday" */ '../views/MyDayPage.vue'),
+  },
+  {
+    path: '/important',
+    name: 'Important',
+    component: () => import(/* webpackChunkName: "important" */ '../views/MyDayPage.vue'),
+  },
+  {
+    path: '/alltasks',
+    name: 'All Tasks',
+    component: () => import(/* webpackChunkName: "alltasks" */ '../views/MyDayPage.vue'),
+  },
+  {
+    path: '/favourite',
+    name: 'Favourite',
+    component: () => import(/* webpackChunkName: "favourite" */ '../views/MyDayPage.vue'),
+  },
+  {
+    path: '/bookmarks',
+    name: 'Bookmarks',
+    component: () => import(/* webpackChunkName: "bookmarks" */ '../views/MyDayPage.vue'),
+  },
+  {
+    path: '/trash',
+    name: 'Trash',
+    component: () => import(/* webpackChunkName: "trash" */ '../views/MyDayPage.vue'),
   },
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+  linkActiveClass: 'selected',
 });
 
 export default router;
