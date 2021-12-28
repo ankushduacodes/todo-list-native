d6061000a65930d6844ef1f765e66a2ecb4a7949
Ankush Dua <ankushduacodes@gmail.com>
2021-12-28 17:38:15 +0530

    added todo list component to render lists of data

---
+++ src/Components/UI/Todo/TodoList.vue
@@ -0,0 +1,35 @@
+<template>
+  <ion-list>
+    <ion-list-header>
+      <h4>{{ header }}</h4>
+    </ion-list-header>
+    <template v-for="todo in todoList" :key="todo.id">
+      <todo-list-item :todo="todo" :checked="header.toLowerCase() === 'done'"></todo-list-item>
+    </template>
+  </ion-list>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+// eslint-disable-next-line import/no-unresolved
+import TodoListItem from '@/Components/UI/Todo/TodoListItem.vue';
+
+export default defineComponent({
+  name: 'TodoList',
+  components: { TodoListItem },
+  props: {
+    header: {
+      type: String,
+      required: true,
+    },
+    todoList: {
+      type: Array,
+      required: true,
+    },
+  },
+});
+</script>
+
+<style scoped>
+
+</style>
