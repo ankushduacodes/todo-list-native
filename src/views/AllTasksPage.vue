ab483b546beda1d9a81998a72022e5dbd549ce91
Ankush Dua <ankushduacodes@gmail.com>
2021-12-30 21:44:07 +0530

    conditional rendering of todos

--- src/views/AllTasksPage.vue
+++ src/views/AllTasksPage.vue
@@ -1,20 +1,36 @@
 <template>
   <ion-page>
     <the-header :heading="heading"></the-header>
+    <ion-content :fullscreen="true">
+      <todo-list v-if="getPending.length" header="Pending" :todo-list="getPending"></todo-list>
+      <todo-list v-if="getDone.length" header="Done" :todo-list="getDone"></todo-list>
+    </ion-content>
   </ion-page>
 </template>
 
 <script>
 import { defineComponent } from 'vue';
-import { IonPage } from '@ionic/vue';
+import { IonPage, IonContent } from '@ionic/vue';
+import { mapGetters } from 'vuex';
 // eslint-disable-next-line import/no-unresolved
 import TheHeader from '@/Components/UI/TheHeader.vue';
+// eslint-disable-next-line import/no-unresolved
+import TodoList from '@/Components/UI/Todo/TodoList.vue';
 
 export default defineComponent({
   name: 'AllTasksPage',
   components: {
     TheHeader,
     IonPage,
+    TodoList,
+    IonContent,
+  },
+  computed: {
+    ...mapGetters({
+      getAll: 'todos/getAllTodos',
+      getDone: 'todos/getDoneAllTodos',
+      getPending: 'todos/getPendingAllTodos',
+    }),
   },
   setup() {
     const heading = 'All Tasks';
