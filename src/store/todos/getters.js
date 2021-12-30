cf7850c352b3f8fd3e6ed299f6d806ecbcd63eda
Ankush Dua <ankushduacodes@gmail.com>
2021-12-30 21:43:55 +0530

    added getters to get filtered todos

--- src/store/todos/getters.js
+++ src/store/todos/getters.js
@@ -1,5 +1,40 @@
-export default {
+const getters = {
   getAllTodos(state) {
     return state.todoList;
   },
+  getDoneAllTodos(state) {
+    return getters.getAllTodos(state).filter((todo) => todo.done);
+  },
+  getPendingAllTodos(state) {
+    return getters.getAllTodos(state).filter((todo) => !todo.done);
+  },
+  getFavTodos(state) {
+    return state.todoList.filter((todo) => todo.favourite);
+  },
+  getDoneFavTodos(state) {
+    return getters.getFavTodos(state).filter((todo) => todo.done);
+  },
+  getPendingFavTodos(state) {
+    return getters.getFavTodos(state).filter((todo) => !todo.done);
+  },
+  getBookmarkTodos(state) {
+    return state.todoList.filter((todo) => todo.bookmark);
+  },
+  getDoneBookmarkTodos(state) {
+    return getters.getBookmarkTodos(state).filter((todo) => todo.done);
+  },
+  getPendingBookmarkTodos(state) {
+    return getters.getBookmarkTodos(state).filter((todo) => !todo.done);
+  },
+  getImportantTodo(state) {
+    return state.todoList.filter((todo) => todo.important);
+  },
+  getDoneImportantTodo(state) {
+    return getters.getImportantTodo(state).filter((todo) => todo.done);
+  },
+  getPendingImportantTodo(state) {
+    return getters.getImportantTodo(state).filter((todo) => !todo.done);
+  },
 };
+
+export default getters;
