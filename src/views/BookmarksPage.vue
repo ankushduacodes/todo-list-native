65881e46e693adb413f69f39d1d592637770b943
Ankush Dua <ankushduacodes@gmail.com>
2022-01-03 22:43:07 +0530

    bookmarks page now works

--- src/views/BookmarksPage.vue
+++ src/views/BookmarksPage.vue
@@ -1,27 +1,42 @@
 <template>
   <ion-page>
     <the-header :heading="heading"></the-header>
+    <ion-content :fullscreen="true">
+      <todo-list v-if="getPending.length" header="Pending" :todo-list="getPending"></todo-list>
+      <todo-list v-if="getDone.length" header="Done" :todo-list="getDone"></todo-list>
+    </ion-content>
   </ion-page>
 </template>
 
 <script>
 import { defineComponent } from 'vue';
-import { IonPage } from '@ionic/vue';
+import { IonPage, IonContent } from '@ionic/vue';
+import { mapGetters } from 'vuex';
 // eslint-disable-next-line import/no-unresolved
 import TheHeader from '@/Components/UI/TheHeader.vue';
+// eslint-disable-next-line import/no-unresolved
+import TodoList from '@/Components/UI/Todo/TodoList.vue';
 
 export default defineComponent({
   name: 'BookmarksPage',
   components: {
     TheHeader,
     IonPage,
+    TodoList,
+    IonContent,
+  },
+  computed: {
+    ...mapGetters({
+      getAll: 'todos/getAllTodos',
+      getDone: 'todos/getDoneBookmarkTodos',
+      getPending: 'todos/getPendingBookmarkTodos',
+    }),
   },
   setup() {
     const heading = 'Bookmarks';
-    return {
-      heading,
-    };
+    return { heading };
   },
+
 });
 </script>
 
