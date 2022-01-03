656f80ec06720c128ba9775dff07e9283875d920
Ankush Dua <ankushduacodes@gmail.com>
2022-01-03 22:42:14 +0530

    muts for done and undone functionality

--- src/store/todos/mutations.js
+++ src/store/todos/mutations.js
@@ -1,7 +1,14 @@
 export default {
   addNewTodo(state, payload) {
     const { newTodo } = payload;
-    console.log('MUTATAION');
     state.todoList.push(newTodo);
+  },
+  markTodoDone(state, payload) {
+    const target = state.todoList.find((todo) => todo.id === payload.todo.id);
+    target.done = true;
+  },
+  markTodoUndone(state, payload) {
+    const target = state.todoList.find((todo) => todo.id === payload.todo.id);
+    target.done = false;
   },
 };
