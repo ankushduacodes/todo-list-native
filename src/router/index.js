fcd0aecf16ed8a635b6a7261754b808ecce62a4b
Ankush Dua <ankushduacodes@gmail.com>
2022-01-05 09:39:43 +0530

    added progress bar while loading rotes

--- src/router/index.js
+++ src/router/index.js
@@ -5,5 +5,20 @@ const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
 });
+router.beforeResolve((to, from, next) => {
+  // If this isn't an initial page load.
+  if (to.name) {
+    // Start the route progress bar.
+    // eslint-disable-next-line no-undef
+    NProgress.start();
+  }
+  next();
+});
+
+router.afterEach(() => {
+  // Complete the animation of the route progress bar.
+  // eslint-disable-next-line no-undef
+  NProgress.done();
+});
 
 export default router;
