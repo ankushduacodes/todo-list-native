64d69e22f63fc16948dd7c07e079d029a1352ba5
Ankush Dua <ankushduacodes@gmail.com>
2022-01-11 13:56:36 +0530

    Reconfigured how main app is shown which is to be renderened based on how auth will work

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,24 @@
 <template>
   <ion-app>
-    <ion-split-pane when="md" content-id="main-content">
-      <side-menu></side-menu>
-      <ion-router-outlet id="main-content"></ion-router-outlet>
-    </ion-split-pane>
+    <component :is="'ion-app-page'"></component>
   </ion-app>
 </template>
 
 <script>
-import {
-  IonApp,
-  IonRouterOutlet,
-  IonSplitPane,
-} from '@ionic/vue';
+
+import { IonApp } from '@ionic/vue';
 import { defineComponent } from 'vue';
-// eslint-disable-next-line import/no-extraneous-dependencies
-import {
-  add,
-} from 'ionicons/icons';
 // eslint-disable-next-line import/no-unresolved
-import SideMenu from '@/Components/UI/SideMenu.vue';
+import IonAppPage from '@/Components/layouts/IonAppPage.vue';
 
 export default defineComponent({
-  name: 'App',
   components: {
-    SideMenu,
+    IonAppPage,
     IonApp,
-    // eslint-disable-next-line vue/no-unused-components
-    IonRouterOutlet,
-    IonSplitPane,
-  },
-  setup() {
-    return {
-      add,
-    };
   },
 });
 </script>
 
 <style>
-:root {
-  --ion-safe-area-top: 20px;
-  --ion-safe-area-bottom: 22px;
-}
+
 </style>
