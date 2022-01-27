f9b47f3129cfbeb4cc68b650b6ff8220a1bf6bfb
Ankush Dua <ankushduacodes@gmail.com>
2022-01-28 01:39:25 +0530

    added axios to the project with its configurations

---
+++ src/api/index.js
@@ -0,0 +1,34 @@
+import axios from 'axios';
+import NProgress from 'nprogress';
+
+const baseURL = 'https://todo-list-app-bknd.herokuapp.com/api/v1/';
+const headers = {
+  Accept: 'application/json',
+  'Content-Type': 'application/json',
+  Authorization: `Bearer ${localStorage.getItem('auth-token')}`,
+};
+// eslint-disable-next-line import/prefer-default-export
+const axiosInstance = axios.create({
+  timeout: 2000,
+  baseURL,
+  headers,
+});
+
+axiosInstance.interceptors.request.use(
+  async (config) => {
+    NProgress.start();
+    return config;
+  },
+  async (error) => {
+    await Promise.reject(error);
+  },
+);
+
+axiosInstance.interceptors.response.use((response) => {
+  NProgress.done();
+  return response;
+}, async (error) => {
+  await Promise.reject(error);
+});
+
+export default axiosInstance;
