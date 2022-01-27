f56c9e7ee32c8e621f0abcd08c0b50826231f0d2
Ankush Dua <ankushduacodes@gmail.com>
2022-01-28 01:41:16 +0530

    added login action to login the user (currently for testing)

--- src/store/auth/actions.js
+++ src/store/auth/actions.js
@@ -1,5 +1,22 @@
+// eslint-disable-next-line import/extensions,import/no-unresolved
+import axiosInstance from '@/api';
+
 export default {
   setLogin({ commit }) {
+    commit('setLoginStatus');
+  },
+  // eslint-disable-next-line no-unused-vars
+  async login({ commit }, payload) {
+    let response;
+    try {
+      response = await axiosInstance.post('user/login', {
+        email: payload.email,
+        password: payload.password,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+    console.log(response);
     commit('setLoginStatus');
   },
 };
